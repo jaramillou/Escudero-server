81c68c593ef8bafcb490916e2c540fb2b92039a2
jaramillou <jaramillou@gmail.com>
2020-11-30 18:41:37 +0100

    primer commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
