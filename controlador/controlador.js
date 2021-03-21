72ee803304b2b4f77817cc7a3d354d7b881fba09
jaramillou <jaramillou@gmail.com>
2021-03-21 07:06:48 +0100

    bottstrap

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -17,11 +17,16 @@ require('../config');
 const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 // parse application/json
 app.use(bodyParser.json())
+
+
+
 
 
 const hbs = require('hbs')
@@ -45,10 +50,7 @@ app.use(require('./amigos.js'))
 
 app.use(express.static('public'));
 
-//app.use(express.static(__dirname + '../public'));
-//hbs.registerPartials(__dirname + '../views/parciales');
-//app.set('view engine', 'hbs');
-//hbs.registerPartials(__dirname + '../views/partials', function(err) {});
+
 
 
 
@@ -79,100 +81,50 @@ app.post('/envio', verificatoken, async(req, res) => { // añade nueva cordenada
 
     console.log("tiempo en: " + this.body.GeoJson[0][1] + ' , ' + this.body.GeoJson[0][0] + " = " + a.dir)
 
-    //var usuario = await Usuario.findOneAndUpdate(req.usuario._id, function(err, usuarioDB) { //falta gestión de usuario...
 
-    // console.log("amigos req :  " + amigos)
-
-    //);
-    //usuario.viento.$push(a.desc)
 
 
 
 
     console.log("usuario: " + this.body.nombre)
     Usuario.findOneAndUpdate({ _id: req.usuario._id }, {
-            $push: {
-                cordenadas: coord,
-                velocidad: vel,
-                velMax: this.body.velMax,
-                hora: this.body.hora,
-                hora_fin: this.body.hora_fin,
-                viento: a.viento,
-                dir: a.dir,
-                temp: a.temp,
-                spot: a.spot,
-                clima: a.clima
+        $push: {
+            cordenadas: coord,
+            velocidad: vel,
+            velMax: this.body.velMax,
+            hora: this.body.hora,
+            hora_fin: this.body.hora_fin,
+            viento: a.viento,
+            dir: a.dir,
+            temp: a.temp,
+            spot: a.spot,
+            clima: a.clima
 
 
 
-            }
-        }, { new: true }, (err, usuarioDB) => {
+        }
+    }, { new: true }, (err, usuarioDB) => {
 
-            if (err) {
+        if (err) {
 
-                console.log(usuarioDB)
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-
-            //console.log("usuario cargadossss: " + usuarioDB)
-
-            console.log("cordenadas añadidas******")
-
-            res.json({
-                ok: true,
-                usuario: usuarioDB
+            console.log("error al grabar coordenadas de usuario" + usuarioDB)
+            return res.status(400).json({
+                ok: false,
+                err
             });
+        }
 
-        })
-        /*
-        Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { velocidad: vel } }, { new: true }, (err, usuarioDB) => {
+        //console.log("usuario cargadossss: " + usuarioDB)
 
-            if (err) {
-                console.log("error en push velocidad")
-                console.log(usuarioDB)
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
+        console.log("cordenadas añadidas******")
+
+        res.json({
+            ok: true,
+            //usuario: usuarioDB
         });
-        Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { velMax: this.body.velMax } }, { new: true }, (err, usuarioDB) => {
 
-            if (err) {
-                console.log("error en push velocidad")
-                console.log(usuarioDB)
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-        });
-        Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { hora: this.body.hora } }, { new: true }, (err, usuarioDB) => {
+    })
 
-            if (err) {
-                console.log("error en push velocidad")
-                console.log(usuarioDB)
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-        });
-        Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { hora_fin: this.body.hora_fin } }, { new: true }, (err, usuarioDB) => {
-
-            if (err) {
-                console.log("error en push velocidad")
-                console.log(usuarioDB)
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-        });
-        */
 });
 
 
@@ -194,7 +146,7 @@ app.post('/borrar/:sesionID', verificatoken, async(req, res) => {
         // console.log("amigos req :  " + amigos)
 
     });
-    console.log("cordenadas  borrado elemento: " + usuario.cordenadas[sesionID])
+
     usuario.cordenadas.splice([sesionID], 1)
     usuario.velocidad.splice(0, 1) //reparar velocidad en matriz como cordenadas
     usuario.hora.splice(sesionID, 1)
@@ -206,77 +158,14 @@ app.post('/borrar/:sesionID', verificatoken, async(req, res) => {
         if (err) {
             console.log(err);
         } else {
-            console.log("cordenadas  borrado elemento: " + usuario.cordenadas)
+            console.log("cordenadas  borrado elemento: ")
         }
 
     });
+    res.json({
+        ok: true,
+    });
 
-    /*
-        var borrado = Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $pull: { cordenadas: {cordenadas.0} } }, (err, usuarioDB) => {
-
-            if (err) {
-
-                console.log("cordenadas no borradas")
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-
-            console.log("cordenadas  borrado elemento: " + usuarioDB)
-
-            res.json({
-                ok: true,
-                usuario: usuarioDB
-            });
-
-        });
-
-        
-            Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { velocidad: vel } }, { new: true }, (err, usuarioDB) => {
-
-                if (err) {
-                    console.log("error en push velocidad")
-                    console.log(usuarioDB)
-                    return res.status(400).json({
-                        ok: false,
-                        err
-                    });
-                }
-            });
-            Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { velMax: this.body.velMax } }, { new: true }, (err, usuarioDB) => {
-
-                if (err) {
-                    console.log("error en push velocidad")
-                    console.log(usuarioDB)
-                    return res.status(400).json({
-                        ok: false,
-                        err
-                    });
-                }
-            });
-            Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { hora: this.body.hora } }, { new: true }, (err, usuarioDB) => {
-
-                if (err) {
-                    console.log("error en push velocidad")
-                    console.log(usuarioDB)
-                    return res.status(400).json({
-                        ok: false,
-                        err
-                    });
-                }
-            });
-            Usuario.findOneAndUpdate({ _id: req.usuario._id }, { $push: { hora_fin: this.body.hora_fin } }, { new: true }, (err, usuarioDB) => {
-
-                if (err) {
-                    console.log("error en push velocidad")
-                    console.log(usuarioDB)
-                    return res.status(400).json({
-                        ok: false,
-                        err
-                    });
-                }
-                */
 });
 
 
--- controlador/navegacion.js
+++ controlador/navegacion.js
@@ -123,63 +123,7 @@ app.get('/muro', verificatoken, async(req, res) => {
 
 
         }
-        /*
-                //  console.log("0 :" + usuarioDB[0].hora[0])
-                //  console.log("1 :" + usuarioDB[1].hora[0])
-                //  console.log("2 :" + usuarioDB[2].hora[0])
-                var n = [] //nos servirá como puntero o iterador de cada usuario amigo
-                var horas = [] //para anotar la hora de cada iterador 
-                var total = 0 //anotaremos el total de sesiones propias y de los amigos
 
-                for (var i = 0; i < amigos.length; i++) {
-                    if (usuarioDB[i]) {
-                        n[i] = usuarioDB[i].hora.length - 1 //llevará la cuenta de los que ya se han puesto en cada vector
-                        total += usuarioDB[i].hora.length //número total de sesiones de los amiguetes
-                        console.log("sesiones de cada usuariuo== " + usuarioDB[i].nombre + " : " + n[i])
-                    }
-                }
-                //console.log("amigos total = " + total)
-                for (var j = total - 1; j > -1; j--) { ///todos los campos de los amigos
-                    for (var i = 0; i < amigos.length; i++) { //cogemos cada amigo
-                        if (n[i] > -1) {
-                            horas[i] = usuarioDB[i].hora[n[i]] //juntamos las horas que quedan
-                                // console.log("horas en usuario== " + usuarioDB[i].nombre + " : " + horas[i])
-                        } else horas[i] = null
-
-
-
-                    }
-                    //[usuarioDB[0].hora[0], usuarioDB[1].hora[0], usuarioDB[2].hora[0]]
-
-                    var m = menor(horas)
-
-
-                    // console.log("menor = " + m)
-
-                    //console.log("tamaño de cordenadas:  " + usuarioDB[m].cordenadas[n[m]].length)
-                    // for (var i = 0; i < usuarioDB[m].cordenadas[n[m]].length; i++) {
-
-                    var coord = new Cordenadas()
-                        // console.log(usuarioDB[m].cordenadas[n[m]])
-                    cordenadas.setCordenadas = usuarioDB[m].cordenadas[n[m]]
-                    var aux = JSON.parse(usuarioDB[m].cordenadas[n[m]])
-                        // var simplified = simplify(aux, 0.000000000000000000000000001)
-
-                    var simplified = simplify(aux, 0.0001, false) //simplifico los datos con el algoritmo Douglas-Peucker para que "quepan" en la URL (~8K) 
-                        //la variable tolerancia 0.0001 debería ser proporcianal al tamaño en la clase Cordenada 
-                        //console.log(simplified)
-                        // cordenadas.simple()
-                        // };
-
-                    URL_geojson += `<div><br><br><h3>usuario: ${usuarioDB[m].nombre},  fecha: ${usuarioDB[m].hora[n[m]]} </h3> `
-                    URL_geojson += `<a href="/sesion/${n[m]}/${usuarioDB[m]._id}"><img src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson(%7B%22type%22%3A%22LineString%22%2C%22coordinates%22%3A${JSON.stringify(simplified)}%7D)/auto/1000x600?access_token=pk.eyJ1IjoiamphcmEiLCJhIjoiY2tkOGpkcDVzMGRuejJyb2RsYmUxcDZubCJ9.7-rob0zcnIsBcmy4SGL-_A"/>
-                    </a></div>`
-                    n[m]--
-                }
-
-
-
-        */
 
 
         res.render('muro.hbs', {
@@ -189,7 +133,7 @@ app.get('/muro', verificatoken, async(req, res) => {
 
         });
 
-        //es.send(URL_geojson)
+
     });
 
 });
